6efd9818981baf050717ae21dc05896150b22e6d
Kim woo seob <rokuta1059@gmail.com>
2020-08-21 19:55:35 +0900

    간단한 인증처리 구현

---
+++ src/main/java/com/springtest/resttest/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.springtest.resttest.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+        throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("kwskws")
+                .password("{noop}test1234")
+                // 권한 지정
+                .roles("USER");
+    }
+
+}
