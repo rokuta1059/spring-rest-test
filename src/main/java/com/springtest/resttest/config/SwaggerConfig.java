c0e1227bf6e1fd031ff23277cc2cdc4908bd9de8
Kim woo seob <rokuta1059@gmail.com>
2020-08-21 16:40:01 +0900

    Swagger Documentation

--- src/main/java/com/springtest/resttest/config/SwaggerConfig.java
+++ src/main/java/com/springtest/resttest/config/SwaggerConfig.java
@@ -8,22 +8,43 @@ import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDi
 import org.springframework.plugin.core.SimplePluginRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
+import java.awt.*;
+import java.util.*;
 import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig extends WebMvcConfigurerAdapter {
 
+    // 연락처 정보 저장을 위한 Contact 객체
+    private static final Contact DEFAULT_CONTACT = new Contact(
+            "Wooseob Kim", "https://github.com/rokuta1059", "rokuta1059@kangwon.ac.kr"
+    );
+
+    private static final ApiInfo DEFAULT_API_INFO = new ApiInfo(
+            "My Own API Title", "My Own User management REST API", "0.1", "urn:tos",
+            DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0", new ArrayList<>()
+    );
+
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<>(
+            Arrays.asList("application/json", "application/xml")
+    );
+
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2);
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(DEFAULT_API_INFO)
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
     }
 
+    // 최신 버전의 경우 아래 내용을 추가해야만 api 문서가 생성된다
     @Bean
     public LinkDiscoverers discoverers() {
         List<LinkDiscoverer> plugins = new ArrayList<>();
--- src/main/java/com/springtest/resttest/user/User.java
+++ src/main/java/com/springtest/resttest/user/User.java
@@ -3,6 +3,8 @@ package com.springtest.resttest.user;
 // import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,22 +21,27 @@ import java.util.Date;
 @NoArgsConstructor
 //@JsonIgnoreProperties(value={"password", "ssn"})
 //@JsonFilter("UserInfo")
+@ApiModel(description="사용자 상세 정보를 위한 도메인 객체")
 public class User {
     private Integer id;
 
     // 유효성 검사: 길이가 최소 2 이상
     @Size(min=2, message="Name은 2글자 이상 입력해 주세요.")
+    @ApiModelProperty(notes="사용자 이름을 입력해주세요")
     private String name;
 
     // 유효성 검사: 현재 시간보다 이전 시간일 것
     @Past
+    @ApiModelProperty(notes="사용자 등록일을 입력해주세요")
     private Date joinDate;
 
     // 외부에 노출시키고 싶지 않은 데이터
     // jackson 패키지에서 불러들여 사용한다
     // @JsonIgnore 어노테이션을 각각 붙여서 사용할수도 있지만
     // class 단위에서 @JsonIgnoreProperties(value={})를 이용할 수도 있다
+    @ApiModelProperty(notes="사용자 패스워드를 입력해주세요")
     private String password;
+    @ApiModelProperty(notes="사용자 주민번호를 입력해주세요")
     private String ssn;
 
 }
